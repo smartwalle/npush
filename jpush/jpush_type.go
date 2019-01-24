cf1479fbebf92cfc10976426c387cd2d19b69f88
smartwalle <smartwalle@gmail.com>
2019-01-24 16:31:10 +0800

    jpush push 接口完成

---
+++ jpush/jpush_type.go
@@ -0,0 +1,21 @@
+package jpush
+
+import (
+	"fmt"
+)
+
+//////////////////////////////////////////////////////////////////////////////////
+type Error struct {
+	Code    int    `json:"code"`
+	Message string `json:"message"`
+}
+
+func (this *Error) Error() string {
+	return fmt.Sprintf("%d-%s", this.Code, this.Message)
+}
+
+//////////////////////////////////////////////////////////////////////////////////
+type CIDListResponse struct {
+	Error   *Error   `json:"error"`
+	CIDList []string `json:"cidlist"`
+}
---
+++ jpush/push_test.go
@@ -0,0 +1,33 @@
+package jpush_test
+
+import (
+	"gitlab.com/smartwalle/push4go/jpush"
+	"testing"
+)
+
+func TestJPush_Push(t *testing.T) {
+	var p = jpush.PushParam{}
+	p.Platform = "all"
+	p.Audience = "all"
+	p.Notification = &jpush.Notification{}
+	p.Notification.Alert = "notification alert"
+
+	p.Notification.Android = &jpush.NotificationAndroid{}
+	p.Notification.Android.Title = "title"
+	p.Notification.Android.BigText = "big text"
+	p.Notification.Android.Style = 1
+
+	p.Notification.IOS = &jpush.NotificationIOS{}
+	p.Notification.IOS.Alert = "ios"
+	p.Notification.IOS.Badge = 1
+
+	p.Options = &jpush.Options{}
+	p.Options.APNSProduction = false
+
+	var rsp, err = client.Push(p)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	t.Log(rsp.MsgId, rsp.SendNo)
+}
